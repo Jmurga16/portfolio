11fcc3ffa26109cbbd44779d14b3169241891435
Jose Murga <murga.jose16@gmail.com>
2024-10-26 18:53:52 -0500

    About Version 1

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  personalInfo = [
+    { tag: "Nombre", description: "José Murga" },
+    { tag: "Carrera", description: "Ingeniería de Sistemas" },
+    { tag: "Teléfono", description: "+51 992692389" },
+    { tag: "Email", description: "jose.murga16@hotmail.com" },
+    { tag: "País", description: "Perú" }
+  ]
+
 }
