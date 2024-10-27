9a0b411640a69f310948775a075ce4a9a573447e
Jose Murga <murga.jose16@gmail.com>
2024-10-27 02:44:14 -0500

    Skills Version 1

--- src/app/pages/skill/skill.component.ts
+++ src/app/pages/skill/skill.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './skill.component.scss'
 })
 export class SkillComponent {
-
+  skillsList = [
+    { name: 'Angular', logo: 'assets/images/skills/angular.svg' },
+    { name: 'JavaScript', logo: 'assets/images/skills/javascript.svg' },
+    { name: 'HTML', logo: 'assets/images/skills/html5.svg' },
+    { name: 'CSS', logo: 'assets/images/skills/css.svg' },
+    { name: 'Node.js', logo: 'assets/images/skills/nodejs.svg' },
+    { name: 'Bootstrap', logo: 'assets/images/skills/bootstrap.svg' }
+    // Añade más habilidades según sea necesario
+  ];
 }
