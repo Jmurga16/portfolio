5386331a6af20c57c8fe9a77d9cf27a34b83ed3f
Jose Murga <murga.jose16@gmail.com>
2024-10-27 02:33:04 -0500

    Proyectos Version 1

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -8,26 +8,46 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   projectsList = [
     {
-      name: 'Proyecto 1',
-      description: 'Descripción breve del proyecto 1',
-      image: 'assets/images/proyecto1.jpg',
-      githubLink: 'https://github.com/usuario/proyecto1',
+      name: 'CRM Legal',
+      description: 'Sistema Web para un Estudio Jurídico hecho en Angular, .NET y SQL Server',
+      image: 'assets/images/projects/lucky.png',
+      githubLink: '',
       demoLink: 'https://proyecto1-demo.com'
     },
     {
-      name: 'Proyecto 1',
-      description: 'Descripción breve del proyecto 1',
-      image: 'assets/images/proyecto1.jpg',
-      githubLink: 'https://github.com/usuario/proyecto1',
+      name: 'Lucky ERP',
+      description: 'Sistema Web para una empresa de Marketing hecho en Angular, .NET y SQL Server',
+      image: 'assets/images/projects/crmlegal.png',
+      githubLink: '',
+      demoLink: 'https://crm-legal.azurewebsites.net/',
+    },
+    {
+      name: 'Sistema web de administracion presupuestal',
+      description: 'Sistema Web hecho en Vue, Java y SQL Server ',
+      image: 'assets/images/projects/referencial.png',
+      githubLink: 'https://github.com/Jmurga16',
       demoLink: 'https://proyecto1-demo.com'
     },
     {
-      name: 'Proyecto 1',
-      description: 'Descripción breve del proyecto 1',
-      image: 'assets/images/proyecto1.jpg',
-      githubLink: 'https://github.com/usuario/proyecto1',
+      name: 'Comprando en Red',
+      description: 'Sistema Web hecho en Angular',
+      image: 'assets/images/projects/referencial.png',
+      githubLink: 'https://github.com/Jmurga16',
       demoLink: 'https://proyecto1-demo.com'
     },
-    // Agrega más proyectos aquí
+    {
+      name: 'Mas Uno',
+      description: 'Landing Page hecho en Angular',
+      image: 'assets/images/projects/referencial.png',
+      githubLink: 'https://github.com/Jmurga16',
+      demoLink: 'https://proyecto1-demo.com'
+    },
+    {
+      name: 'Para Financiera',
+      description: 'Sistema Web hecho en Angular.',
+      image: 'assets/images/projects/referencial.png',
+      githubLink: 'https://github.com/Jmurga16',
+      demoLink: 'https://proyecto1-demo.com'
+    }
   ];
 }
