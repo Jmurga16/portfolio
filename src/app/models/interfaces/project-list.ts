01d45661a397e53687843117cc70ff792f82eccd
Jose Murga <murga.jose16@gmail.com>
2024-10-30 23:07:31 -0500

    Projects v2

---
+++ src/app/models/interfaces/project-list.ts
@@ -0,0 +1,7 @@
+export interface IProjectList {
+    name: string,
+    description: string,
+    image: string,
+    githubLink?: string
+    demoLink?: string
+}
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProjectList } from '../../models/interfaces/project-list';
 
 @Component({
   selector: 'app-projects',
@@ -6,48 +7,90 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-  projectsList = [
+  projectsList: IProjectList[] = [
     {
       name: 'CRM Legal',
       description: 'Sistema Web para un Estudio Jurídico hecho en Angular, .NET y SQL Server',
-      image: 'assets/images/projects/lucky.png',
+      image: 'assets/images/projects/crmlegal.png',
       githubLink: '',
-      demoLink: 'https://proyecto1-demo.com'
+      demoLink: 'https://crm-legal.azurewebsites.net/'
+    },
+    {
+      name: 'Sistema Web para Gestión de Almacen',
+      description: 'Sistema Web para un almacen de Productos Orgánicos hecho en Angular, .NET y SQL Server',
+      image: 'assets/images/projects/crmlegal.png',
+      githubLink: '',
+      demoLink: ''
     },
     {
       name: 'Lucky ERP',
       description: 'Sistema Web para una empresa de Marketing hecho en Angular, .NET y SQL Server',
-      image: 'assets/images/projects/crmlegal.png',
+      image: 'assets/images/projects/lucky.png',
       githubLink: '',
-      demoLink: 'https://crm-legal.azurewebsites.net/',
+      demoLink: 'https://www.grupolucky.com/',
     },
     {
-      name: 'Sistema web de administracion presupuestal',
-      description: 'Sistema Web hecho en Vue, Java y SQL Server ',
-      image: 'assets/images/projects/referencial.png',
-      githubLink: 'https://github.com/Jmurga16',
-      demoLink: 'https://proyecto1-demo.com'
+      name: 'Inventario',
+      description: 'Sistema Web de Inventario hecho en Angular, .NET y SQL Server',
+      image: 'assets/images/projects/inventario.png',
+      githubLink: 'https://github.com/Jmurga16/Inventario.Front',
+      demoLink: '',
     },
     {
-      name: 'Comprando en Red',
-      description: 'Sistema Web hecho en Angular',
-      image: 'assets/images/projects/referencial.png',
-      githubLink: 'https://github.com/Jmurga16',
-      demoLink: 'https://proyecto1-demo.com'
+      name: 'Libros y Asignaturas',
+      description: 'CRUD hecho en Angular, .NET y SQL Server',
+      image: 'assets/images/projects/libros-asignaturas.png',
+      githubLink: 'https://github.com/Jmurga16/Libros-Asignaturas',
+      demoLink: '',
+    },
+    {
+      name: 'Gestor de Alumnos y Notas',
+      description: 'Sistema hecho en Angular, .NET y SQL Server',
+      image: 'assets/images/projects/alumnos-notas.png',
+      githubLink: 'https://github.com/Jmurga16/GestorAlumnos.Front',
+      demoLink: '',
     },
     {
       name: 'Mas Uno',
       description: 'Landing Page hecho en Angular',
-      image: 'assets/images/projects/referencial.png',
-      githubLink: 'https://github.com/Jmurga16',
-      demoLink: 'https://proyecto1-demo.com'
+      image: 'assets/images/projects/masuno.png',
+      githubLink: '',
+      demoLink: ''
+    },
+    {
+      name: 'Generador de Códigos de Descuentos',
+      description: 'Sistema Generador de códigos hecho en Angular',
+      image: 'assets/images/projects/gestor-codigos.png',
+      githubLink: 'https://github.com/Jmurga16/GeneradorCodigos.Front',
+      demoLink: ''
     },
     {
       name: 'Para Financiera',
       description: 'Sistema Web hecho en Angular.',
       image: 'assets/images/projects/referencial.png',
       githubLink: 'https://github.com/Jmurga16',
-      demoLink: 'https://proyecto1-demo.com'
+      demoLink: ''
+    },
+    {
+      name: 'Sistema web de administracion presupuestal',
+      description: 'Sistema Web hecho en Vue, Java y SQL Server ',
+      image: 'assets/images/projects/referencial.png',
+      githubLink: 'https://github.com/Jmurga16',
+      demoLink: ''
+    },
+    {
+      name: 'Sistema de Gestión Administrativa de Posgrado',
+      description: 'Sistema Web hecho en React, Java y Oracle',
+      image: 'assets/images/projects/referencial.png',
+      githubLink: '',
+      demoLink: ''
+    },
+    {
+      name: 'Sistema Web de Agendamiento de Clientes Aquafiltek',
+      description: 'Sistema Web hecho en PHP nativo, Javascript y MySQL',
+      image: 'assets/images/projects/referencial.png',
+      githubLink: '',
+      demoLink: ''
     }
   ];
 }
