27dc03cf919ff7e96ab246eabcf3c5c6918f5cc6
Jose Murga <murga.jose16@gmail.com>
2024-11-04 11:46:35 -0500

    Traduccion About

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,12 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
-  personalInfo = [
+  /* personalInfo = [
     { tag: "Nombre", description: "José Murga" },
     { tag: "Carrera", description: "Ingeniería de Sistemas" },
     { tag: "Teléfono", description: "+51 992692389" },
     { tag: "Email", description: "jose.murga16@hotmail.com" },
     { tag: "País", description: "Perú" }
+  ] */
+
+  personalInfo = [
+    { tag: "personal.tag.name", description: "personal.description.name" },
+    { tag: "personal.tag.profession", description: "personal.description.profession" },
+    { tag: "personal.tag.phone", description: "personal.description.phone" },
+    { tag: "personal.tag.email", description: "personal.description.email" },
+    { tag: "personal.tag.country", description: "personal.description.country" }
   ]
 
 }
