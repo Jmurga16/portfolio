706e06b1b58b967b3acf6c496f4347d531505b74
Jose Murga <murga.jose16@gmail.com>
2025-04-16 23:19:08 -0500

    Update Skills April 2025

--- src/app/pages/skill/skill.component.ts
+++ src/app/pages/skill/skill.component.ts
@@ -11,12 +11,15 @@ export class SkillComponent {
   skillsList: ISkillList = {
     frontend: [
       { name: 'Angular', logo: 'assets/images/skills/angular.svg' },
+      { name: 'React', logo: 'assets/images/skills/react_dark.svg' },
+      { name: 'TypeScript', logo: 'assets/images/skills/typescript.svg' },
       { name: 'HTML', logo: 'assets/images/skills/html5.svg' },
       { name: 'CSS', logo: 'assets/images/skills/css.svg' },
       { name: 'JavaScript', logo: 'assets/images/skills/javascript.svg' },
       { name: 'Bootstrap', logo: 'assets/images/skills/bootstrap.svg' },
       { name: 'Material', logo: 'assets/images/skills/materialui.svg' },
       { name: 'Prime NG', logo: 'assets/images/skills/primeng.svg' },
+      { name: 'Tailwind', logo: 'assets/images/skills/tailwindcss.svg' },
     ],
     backend: [
       { name: '.NET', logo: 'assets/images/skills/dotnet.svg' },
@@ -34,9 +37,14 @@ export class SkillComponent {
     extra: [
       { name: 'Azure', logo: 'assets/images/skills/azure.svg' },
       { name: 'Git', logo: 'assets/images/skills/git.svg' },
+      { name: 'GitHub', logo: 'assets/images/skills/github_dark.svg' },
+      { name: 'GitLab', logo: 'assets/images/skills/gitlab.svg' },
+      { name: 'JWT', logo: 'assets/images/skills/jwt.svg' },
+      { name: 'RxJs', logo: 'assets/images/skills/rxjs.svg' },
       { name: 'Postman', logo: 'assets/images/skills/postman.svg' },
+      { name: 'Digital Ocean', logo: 'assets/images/skills/digitalocean.svg' },
       { name: 'Figma', logo: 'assets/images/skills/figma.svg' },
-      //{ name: 'Adobe XD', logo: 'assets/images/skills/adobe-xd.svg' },
+      { name: 'Adobe XD', logo: 'assets/images/skills/adobe-xd.svg' },
     ],
   };
 }
